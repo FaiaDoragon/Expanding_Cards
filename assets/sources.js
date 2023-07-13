d5682dbe01a9397ff29783e42f3a446953df17c9
Frank Jimenez <frankjimenezp113@gmail.com>
2023-07-13 01:58:15 -0400

    folder assets added: construction.js and sources.js created

---
+++ assets/sources.js
@@ -0,0 +1,22 @@
+export const cards = [
+    {
+        nombre: "Yelan",
+        link: "https://images7.alphacoders.com/124/1240905.png",
+    },
+    {
+        nombre: "Eula",
+        link: "https://images4.alphacoders.com/121/1210584.jpg",
+    },
+    {
+        nombre: "Shenhe",
+        link: "https://initiate.alphacoders.com/images/119/stretched-1920-1080-1192694.jpg?2005",
+    },
+    {
+        nombre: "Kokomi",
+        link: "https://initiate.alphacoders.com/images/129/stretched-1920-1080-1294709.png?8804",
+    },
+    {
+        nombre: "Rosaria",
+        link: "https://initiate.alphacoders.com/images/121/stretched-1920-1080-1210478.jpg?7316",
+    }
+]
