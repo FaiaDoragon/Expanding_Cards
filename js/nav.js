965c81e05c5f1ceea06649704dadadf1dbb56e65
Frank Jimenez <frankjimenezp113@gmail.com>
2023-07-31 10:02:28 -0400

    nav.js modified: functionality of search bar added

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,15 @@ const menuOpen = document.getElementById('open')
 const menuClose = document.getElementById('close')
 const nav = document.getElementById('nav')
 
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn-search')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
+
 //agregamos la funcionalidad de abrir el menu al botton open
 menuOpen.addEventListener('click', () => {
     nav.classList.add('show-nav')
@@ -10,4 +19,5 @@ menuOpen.addEventListener('click', () => {
 //agregamos la funcionalidad de cerrar el menu al botton close
 menuClose.addEventListener('click', () => {
     nav.classList.remove('show-nav')
+    search.classList.remove('active')
 })
